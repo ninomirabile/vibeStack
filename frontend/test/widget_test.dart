283e02ee80f1444c60c6417068aab63f8a3fc22c
ninomirabile <ninomirabile@hotmail.it>
2025-06-28 11:53:14 +0200

    fix 2 pipeline errors

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -1,8 +1,11 @@
-import "package:flutter_test/flutter_test.dart";
-import "package:vibestack_frontend/main.dart";
+// Package imports:
+import 'package:flutter_test/flutter_test.dart';
+
+// Project imports:
+import 'package:vibestack_frontend/main.dart';
 
 void main() {
-  testWidgets("VibeStackApp builds smoke test", (WidgetTester tester) async {
+  testWidgets('VibeStackApp builds smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const VibeStackApp());
     expect(find.byType(VibeStackApp), findsOneWidget);
   });
